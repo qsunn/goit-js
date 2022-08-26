caf20f886212b69b384fd180fbec844ab3d20b26
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-08-26 18:41:34 +0300

    upg

--- goit-js-hw-06/js/task-08.js
+++ goit-js-hw-06/js/task-08.js
@@ -1,17 +1,17 @@
 const form = document.querySelector('.login-form');
-const inputs = [...document.querySelectorAll('input')];
 
-const reset = () => inputs.forEach(input => input.value = '');
 const submitHandler = (event) => {
     event.preventDefault();
-    const validate = inputs.filter(input => input.value.trim()).length === inputs.length;
+    const validate = event.target.elements.email.value.trim() && event.target.elements.email.value.trim();
     if (validate) {
         const data = {
             email: event.target.elements.email.value,
             password: event.target.elements.password.value
         };
         console.log(data);
-        reset();
+        
+        event.target.elements.email.value = '';
+        event.target.elements.password.value = '';
     } else {
         alert('Всі поля повинні бути заповнені.');
     };
