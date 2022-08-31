ccf749787f1a4af41a5eaf8cfd43662d1033231b
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-08-31 19:10:17 +0300

    added hw

---
+++ goit-js-hw-07/js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+import * as basicLightbox from '../node_modules/basiclightbox/src/scripts/main.js';
+
+const gallery = document.querySelector('.gallery');
+
+const generateGallery = gallery => {
+    return gallery
+        .map(item => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+                />
+            </a>
+        </div>
+        `
+        })
+        .join('');
+};
+
+gallery.insertAdjacentHTML('beforeend', generateGallery(galleryItems));
+
+gallery.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG') basicLightbox.create(`<img src="${event.target.getAttribute('data-source')}" width="800" height="600">`).show();
+})
---
+++ goit-js-hw-07/js/02-lightbox.js
@@ -0,0 +1,22 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const generateGallery = gallery => {
+    return gallery
+        .map(item => {
+            return `
+            <a class="gallery__item" href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                alt="${item.description}"
+                />
+            </a>
+        `
+        })
+        .join('');
+};
+gallery.insertAdjacentHTML('beforeend', document.createElement('li').innerHTML = generateGallery(galleryItems));
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
